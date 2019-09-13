dee42d406a60e6ae7431fad4f88a8a82fc5ccfb0
Greg Bernstein <gregb@grotto-networking.com>
2019-09-13 11:37:42 -0700

    Tested protecting user information from students and guests. Instructors can access.

--- test/testUserRoutes.js
+++ test/testUserRoutes.js
@@ -9,41 +9,88 @@ describe("User Routes", function () {
   before(function (done) {
     // Note use of done to deal with async tasks.
     let q = resetUsers();
-    // console.log(`q is a promise? ${q instanceof Promise}`);
-    // console.log(`q is ${JSON.stringify(q)}`);
     q.then(function () {
       done();
     }).catch(done);
   });
 
-  const agent = request.agent(app);  // Used to remember cookies across multiple calls
-  it("Instructor Login", function (done) {
-    agent
-      .put("/login")
-      .set("Accept", "application/json")
-      .send({
-        email: testUsers[0].email,
-        password: testUsers[0].password
-      })
-      .expect("Content-Type", /json/)
-      .expect(function (res) {
-        console.log(`login result: ${JSON.stringify(res.body)}`);
-        console.log(`cookies: ${JSON.stringify(res.headers['set-cookie'])}`);
-      })
-      .expect(200, done);
-  });
+  describe("Instructor Access", function () {
+    const agent = request.agent(app);  // Used to remember cookies across multiple calls
+    it("Instructor Login", function (done) {
+      agent
+        .put("/login")
+        .set("Accept", "application/json")
+        .send({
+          email: testUsers[0].email,
+          password: testUsers[0].password
+        })
+        .expect("Content-Type", /json/)
+        .expect(function (res) {
+          console.log(`login result: ${JSON.stringify(res.body)}`);
+          console.log(`cookies: ${JSON.stringify(res.headers['set-cookie'])}`);
+        })
+        .expect(200, done);
+    });
 
-  it("Instructor Getting all users with json", function (done) {
-    agent
-      .get("/users")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(function (res) {
-        console.log(res.body);
-        console.log(`There are ${res.body.users.length} users`);
-        assert.lengthOf(res.body.users, 35, "There should be 35 users");
-      })
-      .expect(200, done);
-  });
+    it("Instructor Getting all users with json", function (done) {
+      agent
+        .get("/users")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(function (res) {
+          console.log(res.body);
+          console.log(`There are ${res.body.users.length} users`);
+          assert.lengthOf(res.body.users, 35, "There should be 35 users");
+        })
+        .expect(200, done);
+    });
+  })
+
+  describe("Student Access", function () {
+    const agent = request.agent(app);  // Used to remember cookies across multiple calls
+    it("Student Login", function (done) {
+      agent
+        .put("/login")
+        .set("Accept", "application/json")
+        .send({
+          email: testUsers[1].email,
+          password: testUsers[1].password
+        })
+        .expect("Content-Type", /json/)
+        .expect(function (res) {
+          console.log(`login result: ${JSON.stringify(res.body)}`);
+          console.log(`cookies: ${JSON.stringify(res.headers['set-cookie'])}`);
+        })
+        .expect(200, done);
+    });
+
+    it("Student Getting all users with json", function (done) {
+      agent
+        .get("/users")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(function (res) {
+          console.log(res.body);
+        })
+        .expect(401, done);
+    });
+  })
+
+
+  describe("Guest Access", function () {
+    const agent = request.agent(app);  // Used to remember cookies across multiple calls
+    it("Guest Getting all users with json", function (done) {
+      agent
+        .get("/users")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(function (res) {
+          console.log(res.body);
+        })
+        .expect(401, done);
+    });
+  })
+
+
 
 });
